ffa375098c8ffa944f8f5cf9ee607f567916f6e9
Jesse Soldat <jsoldat@hotmail.com>
2016-01-08 12:00:45 -0500

    each time you click on a button the button mark moves above the target button

--- buttonTab/main.js
+++ buttonTab/main.js
@@ -1,15 +1,17 @@
-
+  var btnMarker = getElem("btn_marker");
 
    function getElem(elem) {
     return document.getElementById(elem);
    }
 
    function loadContent(btn) {
+      btnMarker.style.left = btn.offsetLeft+"px";
+      btn_marker.style.width = btn.offsetWidth-2+"px"; 
+
       getElem("mycontent").innerHTML = "This section is content about " + btn.innerHTML +".";
    }
 
   function initMenu() {
-    var btnMarker = getElem("btn_marker");
     btnMarker.style.left = getElem("btn1").offsetLeft+"px";
     btnMarker.style.width = getElem("btn1").offsetWidth-2+"px";
     btnMarker.style.display = "block";
