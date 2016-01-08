93c578426b1cfec59303f19ed7373c21ecfea599
Jesse Soldat <jsoldat@hotmail.com>
2016-01-08 11:52:48 -0500

    button marker start above the first button on page load

--- buttonTab/main.js
+++ buttonTab/main.js
@@ -1,10 +1,18 @@
+
+
    function getElem(elem) {
-    console.log(elem);
     return document.getElementById(elem);
    }
 
    function loadContent(btn) {
-    console.log(btn);
       getElem("mycontent").innerHTML = "This section is content about " + btn.innerHTML +".";
-
    }
+
+  function initMenu() {
+    var btnMarker = getElem("btn_marker");
+    btnMarker.style.left = getElem("btn1").offsetLeft+"px";
+    btnMarker.style.width = getElem("btn1").offsetWidth-2+"px";
+    btnMarker.style.display = "block";
+   }
+
+   window.addEventListener('load', function(event) { initMenu(); });
